145cda09466dfaad2c64eaf50220e24935f5a76a
PanDaring <767741853@qq.com>
2022-01-23 10:35:50 +0800

    拖动卡牌

--- Assets/Scripts/UI/CardSelect.cs
+++ Assets/Scripts/UI/CardSelect.cs
@@ -1,18 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CardSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    GameObject selectedGameObj;//被点击的物体
+    bool isSelected;
+    Vector2 mousePositionOnScreen;
+    Vector2 mousePositionInWorld;
+    Vector2 formalPos;
+
+    private void Start()
     {
-        
+        selectedGameObj = new GameObject();
+        isSelected = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MouseFollow();
+        //射线检测
+        RaycastHit2D hit = Physics2D.Raycast(mousePositionInWorld, Vector2.zero);
+        if(hit.collider != null)
+        {
+            selectedGameObj = hit.collider.gameObject.transform.parent.gameObject;
+            if(selectedGameObj.layer == 8)
+            {
+                
+            }
+        }
+        //点击操作处理
+        if(Input.GetMouseButtonDown(0))
+        {
+            //第八层为“CardImg”
+            if(selectedGameObj.layer == 8)
+            {
+                formalPos = selectedGameObj.transform.position;
+                isSelected = true;
+                //Debug.Log(selectedGameObj.name);
+            }  
+        }
+        //松开鼠标操作
+        if(Input.GetMouseButtonUp(0))
+        {
+            if(selectedGameObj.layer == 8)
+            {
+                isSelected = false;
+                selectedGameObj.transform.position = formalPos;
+            }
+        }
+
+        //跟随效果
+        if(isSelected)
+        {
+            selectedGameObj.transform.position = mousePositionInWorld;
+        }
     }
+
+    private void MouseFollow()
+    {
+        //获取鼠标在场景中坐标
+        mousePositionOnScreen = Input.mousePosition;
+
+        //将相机中的坐标转化为世界坐标
+        mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
+
+        //物体跟随鼠标移动
+        //transform.position = mousePositionInWorld;
+    }
+
 }
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public static bool cardSelected = false;//判断是否已经选择卡牌
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
