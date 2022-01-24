8a388f1373a8e5c05cd6998b1e2b0b1ca7f37251
PanDaring <767741853@qq.com>
2022-01-24 16:55:39 +0800

    卡牌选择功能

---
+++ Assets/Scripts/UI/CardFrame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardFrame : MonoBehaviour
+{
+    public GameObject frame;
+    private void OnMouseEnter()
+    {
+        if(CardSelect.isSelected == false && CardSelect.isShowcased == false)
+            frame.SetActive(true);
+    }
+    private void OnMouseExit()
+    {
+        if (CardSelect.isSelected == false && CardSelect.isShowcased == false)
+            frame.SetActive(false);
+    }
+    private void Update()
+    {
+        if (CardSelect.isShowcased)
+            frame.SetActive(false);
+    }
+}
--- Assets/Scripts/UI/CardSelect.cs
+++ Assets/Scripts/UI/CardSelect.cs
@@ -1,54 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class CardSelect : MonoBehaviour
 {
-    GameObject selectedGameObj;//被点击的物体
-    bool isSelected;
+    public static GameObject selectedGameObj;//被点击的物体
+    public static GameObject nullGameObj;//为避免报错设定的物体
+    public static bool isShowcased;//是否进入卡牌详情界面
+    public static bool isSelected;
     Vector2 mousePositionOnScreen;
-    Vector2 mousePositionInWorld;
+    public static Vector2 mousePositionInWorld;
     Vector2 formalPos;
+    public static List<AbstractCard> cards;//当前卡牌的序列
+
+    public GameObject showcase;
 
     private void Start()
     {
         selectedGameObj = new GameObject();
+        nullGameObj = selectedGameObj;
+        isShowcased = false;
         isSelected = false;
     }
 
+    [System.Obsolete]
     private void Update()
     {
         MouseFollow();
-        //射线检测
-        RaycastHit2D hit = Physics2D.Raycast(mousePositionInWorld, Vector2.zero);
-        if(hit.collider != null)
+        
+        if(Input.GetMouseButtonDown(1))
         {
-            selectedGameObj = hit.collider.gameObject.transform.parent.gameObject;
-            if(selectedGameObj.layer == 8)
+            RaycastHit2D hit = Physics2D.Raycast(mousePositionInWorld, Vector2.zero);
+
+            if(hit.collider != null)
             {
-                
+                selectedGameObj = hit.collider.gameObject;
             }
+            isShowcased =!isShowcased;
+            //卡牌展示界面
+            showcase.SetActive(!showcase.active);
+            string selectedCardName;
+            //selectedCardNum = selectedGameObj.GetComponent<AbstractCard>().cardNum;//获取卡牌序号
+            selectedCardName = selectedGameObj.name;
+
+            Sprite _cardPic = Resources.Load<Sprite>(selectedCardName);
+            showcase.GetComponent<SpriteRenderer>().sprite = _cardPic;
         }
-        //点击操作处理
-        if(Input.GetMouseButtonDown(0))
+
+        //判断是否进入卡牌展示界面
+        if(isShowcased)
         {
-            //第八层为“CardImg”
-            if(selectedGameObj.layer == 8)
-            {
-                formalPos = selectedGameObj.transform.position;
-                isSelected = true;
-                //Debug.Log(selectedGameObj.name);
-            }  
+            
         }
-        //松开鼠标操作
-        if(Input.GetMouseButtonUp(0))
+        else
         {
-            if(selectedGameObj.layer == 8)
+            //点击操作处理
+            if (Input.GetMouseButtonDown(0))
             {
-                isSelected = false;
-                selectedGameObj.transform.position = formalPos;
+                //射线检测
+                RaycastHit2D hit = Physics2D.Raycast(mousePositionInWorld, Vector2.zero);
+
+                //当未选中卡牌且射线检测到物体，更新已选卡牌
+                if (hit.collider != null && !isSelected)
+                {
+                    selectedGameObj = hit.collider.gameObject;
+                }
+
+                //第八层为“CardImg”
+                if (selectedGameObj.layer == 8)
+                {
+                    formalPos = selectedGameObj.transform.position;
+                    isSelected = true;
+                }
+            }
+            //松开鼠标操作
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (selectedGameObj.layer == 8)
+                {
+                    isSelected = false;
+                    selectedGameObj.transform.position = formalPos;
+                    selectedGameObj = nullGameObj;
+                }
             }
         }
 
@@ -70,5 +103,4 @@ public class CardSelect : MonoBehaviour
         //物体跟随鼠标移动
         //transform.position = mousePositionInWorld;
     }
-
 }
