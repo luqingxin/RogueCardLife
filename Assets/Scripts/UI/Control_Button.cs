b1900d9b2308c3d02edaf88fb1783379b2057fce
PanDaring <767741853@qq.com>
2022-04-27 18:19:56 +0800

    添加按钮

---
+++ Assets/Scripts/UI/Control_Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_Button : MonoBehaviour
+{
+    public GameObject targetObj;
+    private bool selected = false;
+    private int add = 0;
+
+    public void OnClick()
+    {
+        selected = !selected;
+        targetObj.GetComponent<Canvas>().enabled = !targetObj.GetComponent<Canvas>().enabled;
+        if (selected)
+            add = -2;
+        else
+            add = 2;
+
+        transform.position = new Vector3(transform.position.x, transform.position.y+add, transform.position.z);
+    }
+}
