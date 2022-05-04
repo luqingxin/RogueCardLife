0660f5d937d4738362193ac96a7d76d3c4220b7a
luqingxin <3346717758@qq.com>
2022-05-04 21:38:51 +0800

    修改UI

--- Assets/Scripts/UI/CharacterSlider.cs
+++ Assets/Scripts/UI/CharacterSlider.cs
@@ -7,6 +7,7 @@ public class CharacterSlider : MonoBehaviour
 {
     private Slider slider;
     private Text num;
+    public int sliderOrderNum;
 
     private void Awake()
     {
@@ -14,8 +15,27 @@ public class CharacterSlider : MonoBehaviour
         slider = transform.GetChild(2).GetComponent<Slider>();
     }
 
+    private void Update()
+    {
+        DataUpdate();
+    }
+
     public void DataUpdate()
     {
-        //
+        switch (sliderOrderNum)
+        {
+            case 0:
+                num.text = AbstractGameRun.gameRun.playerCharacter.strength.ToString();
+                break;
+            case 1:
+                num.text = AbstractGameRun.gameRun.playerCharacter.move.ToString();
+                break;
+            case 2:
+                num.text = AbstractGameRun.gameRun.playerCharacter.wisdom.ToString();
+                break;
+            case 3:
+                num.text = AbstractGameRun.gameRun.playerCharacter.communication.ToString();
+                break;
+        }
     }
 }
