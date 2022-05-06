c0bf37acf909fd806ab0755355c36645e3e1dba9
luqingxin <3346717758@qq.com>
2022-05-06 21:33:02 +0800

    没啥

---
+++ Assets/Scripts/UI/CardRewardPosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardRewardPosition : MonoBehaviour
+{
+    public static List<Vector2> positions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = new List<Vector2>();
+        GetPos();
+    }
+
+    public void GetPos()
+    {
+        int i = transform.childCount;
+        for (int j = 0; j < i; j++)
+        {
+            positions.Add(transform.GetChild(j).position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
